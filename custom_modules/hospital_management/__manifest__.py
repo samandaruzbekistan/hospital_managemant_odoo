a07f679618adabacd6df808a2c03458bde7cdd1c
Samandar Sariboyev <samandarsariboyev69@gmail.com>
2024-05-26 02:09:32 +0500

    Initial commit

---
+++ custom_modules/hospital_management/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name' : 'Hospital Management',
+    'version' :'1.0.0',
+    'author' : 'Samandar Sariboyev',
+    'website' : 'goldapps.uz',
+    'category' : 'Health',
+    'summary' : 'Hospital Managemant system',
+    'description' : 'Hospital Managemant system',
+    'license' : 'LGPL-3',
+    'application' : True,
+}
